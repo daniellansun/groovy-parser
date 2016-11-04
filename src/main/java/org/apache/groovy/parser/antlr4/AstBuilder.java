24e3285d3cd51b73f56516487237e8377ea9f20f
Daniel.Sun <realbluesun@hotmail.com>
2016-11-04 23:00:07 +0800

    Minor refactoring

--- src/main/java/org/apache/groovy/parser/antlr4/AstBuilder.java
+++ src/main/java/org/apache/groovy/parser/antlr4/AstBuilder.java
@@ -20,7 +20,6 @@ package org.apache.groovy.parser.antlr4;
 
 import groovy.lang.IntRange;
 import org.antlr.v4.runtime.*;
-import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.atn.PredictionMode;
 import org.antlr.v4.runtime.misc.ParseCancellationException;
 import org.antlr.v4.runtime.tree.ParseTree;
@@ -38,7 +37,9 @@ import org.codehaus.groovy.control.SourceUnit;
 import org.codehaus.groovy.control.messages.SyntaxErrorMessage;
 import org.codehaus.groovy.runtime.IOGroovyMethods;
 import org.codehaus.groovy.runtime.StringGroovyMethods;
-import org.codehaus.groovy.syntax.*;
+import org.codehaus.groovy.syntax.Numbers;
+import org.codehaus.groovy.syntax.SyntaxException;
+import org.codehaus.groovy.syntax.Types;
 import org.objectweb.asm.Opcodes;
 
 import java.io.BufferedReader;
@@ -76,6 +77,8 @@ public class AstBuilder extends GroovyParserBaseVisitor<Object> implements Groov
                 new CommonTokenStream(this.lexer));
 
         this.parser.setErrorHandler(new DescriptiveErrorStrategy());
+
+        this.tryWithResourcesASTTransformation = new TryWithResourcesASTTransformation(this);
     }
 
     private GroovyParserRuleContext buildCST(PredictionMode predictionMode) {
@@ -413,318 +416,10 @@ public class AstBuilder extends GroovyParserBaseVisitor<Object> implements Groov
                 })
                 .forEach(tryCatchStatement::addCatch);
 
-        return this.transformTryCatchStatement(this.configureAST(tryCatchStatement, ctx));
-    }
-
-    private boolean isBasicTryWithResourcesStatement(TryCatchStatement tryCatchStatement) {
-        if (EmptyStatement.INSTANCE.equals(tryCatchStatement.getFinallyStatement())
-                && !asBoolean(tryCatchStatement.getCatchStatements())) {
-            return true;
-        }
-
-        return false;
-    }
-
-    private ExpressionStatement makeVariableDeclarationFinal(ExpressionStatement variableDeclaration) {
-        if (!asBoolean(variableDeclaration)) {
-            return variableDeclaration;
-        }
-
-        if (!(variableDeclaration.getExpression() instanceof DeclarationExpression)) {
-            throw new IllegalArgumentException("variableDeclaration is not a declaration statement");
-        }
-
-        DeclarationExpression declarationExpression = (DeclarationExpression) variableDeclaration.getExpression();
-        if (!(declarationExpression.getLeftExpression() instanceof VariableExpression)) {
-            throw createParsingFailedException("The expression statement is not a variable delcaration statement", variableDeclaration);
-        }
-
-        VariableExpression variableExpression = (VariableExpression) declarationExpression.getLeftExpression();
-        variableExpression.setModifiers(variableExpression.getModifiers() | Opcodes.ACC_FINAL);
-
-        return variableDeclaration;
-    }
-
-    private long primaryExcCnt = 0;
-    private String genPrimaryExcName() {
-        return "__$$primaryExc" + primaryExcCnt++;
-    }
-
-    /**
-     * Reference JLS "14.20.3. try-with-resources"(https://docs.oracle.com/javase/specs/jls/se7/html/jls-14.html)
-     *
-     * @param tryCatchStatement the try-with-resources statement to transform
-     * @return try-catch-finally statement, which contains no resources clause
-     */
-    private Statement transformTryCatchStatement(TryCatchStatement tryCatchStatement) {
-        if (!asBoolean(tryCatchStatement.getResourceStatements())) {
-            return tryCatchStatement;
-        }
-
-        if (this.isBasicTryWithResourcesStatement(tryCatchStatement)) {
-            return this.transformBasicTryWithResourcesStatement(tryCatchStatement);
-        } else {
-            return this.transformExtendedTryWithResourcesStatement(tryCatchStatement);
-        }
-    }
-
-    /*
-     *   try ResourceSpecification
-     *       Block
-     *   Catchesopt
-     *   Finallyopt
-     *
-     *   **The above AST should be transformed to the following AST**
-     *
-     *   try {
-     *       try ResourceSpecification
-     *           Block
-     *   }
-     *   Catchesopt
-     *   Finallyopt
-     */
-    private Statement transformExtendedTryWithResourcesStatement(TryCatchStatement tryCatchStatement) {
-        /*
-         *  try ResourceSpecification
-         *      Block
-         */
-        TryCatchStatement newTryWithResourcesStatement =
-                new TryCatchStatement(
-                        tryCatchStatement.getTryStatement(),
-                        EmptyStatement.INSTANCE);
-        tryCatchStatement.getResourceStatements().forEach(newTryWithResourcesStatement::addResource);
-
-
-        /*
-         *   try {
-         *       << the following try-with-resources has been transformed >>
-         *       try ResourceSpecification
-         *           Block
-         *   }
-         *   Catchesopt
-         *   Finallyopt
-         */
-        TryCatchStatement newTryCatchStatement =
-                new TryCatchStatement(
-                        this.createBlockStatement(this.transformTryCatchStatement(newTryWithResourcesStatement)),
-                        tryCatchStatement.getFinallyStatement());
-
-        tryCatchStatement.getCatchStatements().forEach(newTryCatchStatement::addCatch);
-
-        return newTryCatchStatement;
-    }
-
-    /*
-     *   try (VariableModifiersopt R Identifier = Expression ...)
-     *      Block
-     *
-     *  **The above AST should be transformed to the following AST**
-     *
-     *   {
-     *       final VariableModifiers_minus_final R Identifier = Expression;
-     *       Throwable #primaryExc = null;
-     *
-     *       try ResourceSpecification_tail
-     *           Block
-     *       catch (Throwable #t) {
-     *           #primaryExc = #t;
-     *           throw #t;
-     *       } finally {
-     *           if (Identifier != null) {
-     *               if (#primaryExc != null) {
-     *                   try {
-     *                       Identifier.close();
-     *                   } catch (Throwable #suppressedExc) {
-     *                       #primaryExc.addSuppressed(#suppressedExc);
-     *                   }
-     *               } else {
-     *                   Identifier.close();
-     *               }
-     *           }
-     *       }
-     *   }
-     *
-     */
-    private Statement transformBasicTryWithResourcesStatement(TryCatchStatement tryCatchStatement) {
-        // { ... }
-        BlockStatement blockStatement = new BlockStatement();
-
-        // final VariableModifiers_minus_final R Identifier = Expression;
-        ExpressionStatement firstResourceStatement =
-                this.makeVariableDeclarationFinal(
-                        tryCatchStatement.getResourceStatement(0));
-        this.appendStatementsToBlockStatement(blockStatement, firstResourceStatement);
-
-        // Throwable #primaryExc = null;
-        String primaryExcName = this.genPrimaryExcName();
-        ExpressionStatement primaryExcDeclarationStatement =
-                new ExpressionStatement(
-                        new DeclarationExpression(
-                                new VariableExpression(primaryExcName, ClassHelper.make(Throwable.class)),
-                                org.codehaus.groovy.syntax.Token.newSymbol(Types.ASSIGN, -1, -1),
-                                new ConstantExpression(null)
-                        )
-                );
-        this.appendStatementsToBlockStatement(blockStatement, primaryExcDeclarationStatement);
-
-
-        // The generated try-catch-finally statement
-        String firstResourceIdentifierName =
-                ((DeclarationExpression) tryCatchStatement.getResourceStatement(0).getExpression()).getLeftExpression().getText();
-
-        TryCatchStatement newTryCatchStatement =
-                new TryCatchStatement(
-                        tryCatchStatement.getTryStatement(),
-                        this.createFinallyBlockForNewTryCatchStatement(primaryExcName, firstResourceIdentifierName));
-
-        List<ExpressionStatement> resourceStatements = tryCatchStatement.getResourceStatements();
-        // 2nd, 3rd, ..., n'th resources declared in resources
-        List<ExpressionStatement> tailResourceStatements = resourceStatements.subList(1, resourceStatements.size());
-        tailResourceStatements.stream().forEach(newTryCatchStatement::addResource);
-
-        newTryCatchStatement.addCatch(this.createCatchBlockForOuterNewTryCatchStatement(primaryExcName));
-        this.appendStatementsToBlockStatement(blockStatement, this.transformTryCatchStatement(newTryCatchStatement));
-
-
-        return this.configureAST(blockStatement, tryCatchStatement);
-    }
-
-    /*
-     *   catch (Throwable #t) {
-     *       #primaryExc = #t;
-     *       throw #t;
-     *   }
-     *
-     */
-    private CatchStatement createCatchBlockForOuterNewTryCatchStatement(String primaryExcName) {
-        // { ... }
-        BlockStatement blockStatement = new BlockStatement();
-        String tExcName = this.genTExcName();
-
-        // #primaryExc = #t;
-        ExpressionStatement primaryExcAssignStatement =
-                new ExpressionStatement(
-                        new BinaryExpression(
-                                new VariableExpression(primaryExcName),
-                                org.codehaus.groovy.syntax.Token.newSymbol(Types.ASSIGN, -1, -1),
-                                new VariableExpression(tExcName)));
-        this.appendStatementsToBlockStatement(blockStatement, primaryExcAssignStatement);
-
-        // throw #t;
-        ThrowStatement throwTExcStatement = new ThrowStatement(new VariableExpression(tExcName));
-        this.appendStatementsToBlockStatement(blockStatement, throwTExcStatement);
-
-        // Throwable #t
-        Parameter tExcParameter = new Parameter(ClassHelper.make(Throwable.class), tExcName);
-
-        return new CatchStatement(tExcParameter, blockStatement);
-    }
-
-    private long tExcCnt = 0;
-    private String genTExcName() {
-        return "__$$t" + tExcCnt++;
-    }
-
-    /*
-     *   finally {
-     *       if (Identifier != null) {
-     *           if (#primaryExc != null) {
-     *              try {
-     *                  Identifier.close();
-     *              } catch (Throwable #suppressedExc) {
-     *                  #primaryExc.addSuppressed(#suppressedExc);
-     *              }
-     *           } else {
-     *               Identifier.close();
-     *           }
-     *       }
-     *   }
-     *
-     * We can simplify the above code to a Groovy version as follows:
-     *
-     *   finally {
-     *      if (#primaryExc != null)
-     *         try {
-     *             Identifier?.close();
-     *         } catch (Throwable #suppressedExc) {
-     *             #primaryExc.addSuppressed(#suppressedExc);
-     *         }
-     *      else
-     *          Identifier?.close();
-     *
-     *   }
-     *
-     */
-    private BlockStatement createFinallyBlockForNewTryCatchStatement(String primaryExcName, String firstResourceIdentifierName) {
-        BlockStatement finallyBlock = new BlockStatement();
-
-        // primaryExc != null
-        BooleanExpression conditionExpression =
-                new BooleanExpression(
-                        new BinaryExpression(
-                                new VariableExpression(primaryExcName),
-                                org.codehaus.groovy.syntax.Token.newSymbol(Types.COMPARE_NOT_EQUAL, -1, -1),
-                                new ConstantExpression(null)));
-
-        // try-catch statement
-        TryCatchStatement newTryCatchStatement =
-                new TryCatchStatement(
-                        this.createBlockStatement(this.createCloseResourceStatement(firstResourceIdentifierName)), // { Identifier?.close(); }
-                        EmptyStatement.INSTANCE);
-
-
-        String suppressedExcName = this.genSuppressedExcName();
-        newTryCatchStatement.addCatch(
-                // catch (Throwable #suppressedExc) { .. }
-                new CatchStatement(
-                        new Parameter(ClassHelper.make(Throwable.class), suppressedExcName),
-                        this.createBlockStatement(this.createAddSuppressedStatement(primaryExcName, suppressedExcName)) // #primaryExc.addSuppressed(#suppressedExc);
-                )
-        );
-
-        // if (#primaryExc != null) { ... }
-        IfStatement ifStatement =
-                new IfStatement(
-                        conditionExpression,
-                        newTryCatchStatement,
-                        this.createCloseResourceStatement(firstResourceIdentifierName) // Identifier?.close();
-                );
-        this.appendStatementsToBlockStatement(finallyBlock, ifStatement);
-
-        return this.createBlockStatement(finallyBlock);
-    }
-
-    private long suppressedExcCnt = 0;
-    private String genSuppressedExcName() {
-        return "__$$suppressedExc" + suppressedExcCnt++;
-    }
-
-    /*
-     *  Identifier?.close();
-     */
-    private ExpressionStatement createCloseResourceStatement(String firstResourceIdentifierName) {
-        MethodCallExpression closeMethodCallExpression =
-                new MethodCallExpression(new VariableExpression(firstResourceIdentifierName), "close", new ArgumentListExpression());
-
-        closeMethodCallExpression.setImplicitThis(false);
-        closeMethodCallExpression.setSafe(true);
-
-        return new ExpressionStatement(closeMethodCallExpression);
-    }
-
-    /*
-     *  #primaryExc.addSuppressed(#suppressedExc);
-     */
-    private ExpressionStatement createAddSuppressedStatement(String primaryExcName, String suppressedExcName) {
-        MethodCallExpression addSuppressedMethodCallExpression =
-                new MethodCallExpression(
-                        new VariableExpression(primaryExcName),
-                        "addSuppressed",
-                        new ArgumentListExpression(Collections.singletonList(new VariableExpression(suppressedExcName))));
-        addSuppressedMethodCallExpression.setImplicitThis(false);
-        addSuppressedMethodCallExpression.setSafe(true);
-
-        return new ExpressionStatement(addSuppressedMethodCallExpression);
+        return this.configureAST(
+                tryWithResourcesASTTransformation.transformTryCatchStatement(
+                        this.configureAST(tryCatchStatement, ctx)),
+                ctx);
     }
 
 
@@ -3819,7 +3514,7 @@ public class AstBuilder extends GroovyParserBaseVisitor<Object> implements Groov
         return statement;
     }
 
-    private BlockStatement createBlockStatement(Statement... statements) {
+    public BlockStatement createBlockStatement(Statement... statements) {
         return this.createBlockStatement(Arrays.asList(statements));
     }
 
@@ -3827,7 +3522,7 @@ public class AstBuilder extends GroovyParserBaseVisitor<Object> implements Groov
         return this.appendStatementsToBlockStatement(new BlockStatement(), statementList);
     }
 
-    private BlockStatement appendStatementsToBlockStatement(BlockStatement bs, Statement... statements) {
+    public BlockStatement appendStatementsToBlockStatement(BlockStatement bs, Statement... statements) {
         return this.appendStatementsToBlockStatement(bs, Arrays.asList(statements));
     }
 
@@ -4103,7 +3798,7 @@ public class AstBuilder extends GroovyParserBaseVisitor<Object> implements Groov
                         ctx.stop.getCharPositionInLine() + 1 + ctx.stop.getText().length()));
     }
 
-    private CompilationFailedException createParsingFailedException(String msg, ASTNode node) {
+    public CompilationFailedException createParsingFailedException(String msg, ASTNode node) {
         Objects.requireNonNull(node, "node passed into createParsingFailedException should not be null");
 
         return createParsingFailedException(
@@ -4593,6 +4288,7 @@ public class AstBuilder extends GroovyParserBaseVisitor<Object> implements Groov
     private final ClassLoader classLoader; // Our ClassLoader, which provides information on external types
     private final GroovyLangLexer lexer;
     private final GroovyLangParser parser;
+    private final TryWithResourcesASTTransformation tryWithResourcesASTTransformation;
     private final List<ClassNode> classNodeList = new LinkedList<>();
     private final Deque<ClassNode> classNodeStack = new ArrayDeque<>();
     private final Deque<List<InnerClassNode>> anonymousInnerClassesDefinedInMethodStack = new ArrayDeque<>();
