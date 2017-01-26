704420ed6502e75b92e23bcf0998c6af5ad06fb9
Daniel Sun <realbluesun@hotmail.com>
2017-01-26 17:29:50 +0800

    Add more test cases for "implies" operator

--- src/test/resources/core/ImpliesOp_01x.groovy
+++ src/test/resources/core/ImpliesOp_01x.groovy
@@ -7,3 +7,12 @@ assert !(1 == 1 => 2 == 3)
 assert 1 == 1 => 1 == 1
 assert 2 == 3 => 1 == 1
 assert 2 == 3 => 2 == 3
+
+assert true => true => true
+assert !(true => true => false)
+assert true => false => true
+assert true => false => false
+assert false => true => true
+assert !(false => true => false)
+assert false => false => true
+assert !(false => false => false)
