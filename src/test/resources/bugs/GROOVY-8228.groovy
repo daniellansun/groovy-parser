15d72bb029383ec429fb584444e961e20301cc6e
Daniel Sun <realbluesun@hotmail.com>
2017-06-18 19:48:01 +0800

    Refine the test

--- src/test/resources/bugs/GROOVY-8228.groovy
+++ src/test/resources/bugs/GROOVY-8228.groovy
@@ -1,3 +1,21 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
 import java.lang.annotation.Retention
 import java.lang.annotation.Target
 import java.lang.reflect.Field
@@ -20,7 +38,7 @@ interface JSR308Interface2<T extends @JSR308 CharSequence> {}
 class JSR308Class extends @JSR308 JSR308BaseClass<@JSR308 List> implements @JSR308 JSR308Interface1<@JSR308 String>, @JSR308 JSR308Interface2<@JSR308 String> {
     @JSR308 private  String name;
 
-    @JSR308 List<@JSR308 String> test(@JSR308 List<? extends @JSR308 Object> list) throws @JSR308 IOException, @JSR308 java.sql.SQLException {
+    @JSR308 List<@JSR308 String> test(@JSR308 List<@JSR308 ? extends @JSR308 Object> list) throws @JSR308 IOException, @JSR308 java.sql.SQLException {
         @JSR308 List<@JSR308 String> localVar = new @JSR308 ArrayList<@JSR308 String>();
 
         try {
