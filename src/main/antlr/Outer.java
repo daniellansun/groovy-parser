ebf58526faa9dcc10cef83a5e051c068bc3c0bf0
Daniel Sun <realbluesun@hotmail.com>
2018-04-12 08:23:49 +0800

    Clean useless code
    (cherry picked from commit 2ff0d01)

--- src/main/antlr/Outer.java
+++
@@ -1,9 +0,0 @@
-public class Outer {
-    public class Inner {
-        public String name;
-    }
-
-    public static String create(Outer outer) {
-        return outer.new Inner().name;
-    }
-}
