4f59b95a8b81ad7191db1b2e600aeffab502b11f
danielsun1106 <realbluesun@hotmail.com>
2018-04-14 21:23:47 +0800

    GROOVY-8544: The new parser Parrot does not support enum constants end with semicolon

--- src/test/resources/core/EnumDeclaration_01.groovy
+++ src/test/resources/core/EnumDeclaration_01.groovy
@@ -1,3 +1,21 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
 enum AAAA {}
 public enum AAAA2 {}
 public enum AAAA3 implements A {}
@@ -5,6 +23,11 @@ public enum AAAA3 implements A {}
 public enum AAAA4 implements A,B {}
 enum BBBB {A}
 enum CCCC {A, B}
+enum CCCC2 {A, B
+}
+enum CCCC3 {
+    A, B
+}
 enum DDDD {A, B,}
 enum EEEE {
     A,
@@ -42,3 +65,10 @@ enum E3 {
 
 
 enum e {}
+
+enum Enum1 {
+    A, B;
+}
+enum Enum2 {
+    A, B;}
+enum Enum3 {A, B;}
