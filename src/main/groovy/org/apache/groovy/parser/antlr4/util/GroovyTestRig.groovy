c238a79baf75c1e2bfbbc3678c17d985f15b923a
Daniel Sun <sunlan@apache.org>
2019-02-10 03:09:51 +0800

    Support profiling parser

--- src/main/groovy/org/apache/groovy/parser/antlr4/util/GroovyTestRig.groovy
+++ src/main/groovy/org/apache/groovy/parser/antlr4/util/GroovyTestRig.groovy
@@ -23,6 +23,9 @@ import org.antlr.v4.gui.TestRig
 import org.antlr.v4.runtime.CharStream
 import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
+import org.antlr.v4.runtime.atn.ATNSimulator
+import org.antlr.v4.runtime.atn.DecisionInfo
+import org.antlr.v4.runtime.atn.ProfilingATNSimulator
 import org.apache.groovy.parser.antlr4.GroovyLangLexer
 import org.apache.groovy.parser.antlr4.GroovyLangParser
 
@@ -33,44 +36,69 @@ import org.apache.groovy.parser.antlr4.GroovyLangParser
  * Created on    2016/08/14
  */
 @Log
-public class GroovyTestRig extends TestRig {
-    public GroovyTestRig(String[] args) throws Exception {
-        super(['Groovy', args.contains('-lexer') ? 'tokens' : 'compilationUnit', *args] as String[]);
+class GroovyTestRig extends TestRig {
+    GroovyTestRig(String[] args) throws Exception {
+        super(['Groovy', args.contains('-lexer') ? 'tokens' : 'compilationUnit', *args] as String[])
     }
 
-    public void inspectParseTree() {
-        def inputFile = new File(this.inputFiles[0]);
+    void inspectParseTree() {
+        def inputFile = new File(this.inputFiles[0])
 
         if (!(inputFile.exists() && inputFile.isFile())) {
             log.info "Input file[${inputFile.absolutePath}] does not exist."
-            return;
+            return
         }
 
-        byte[] content = inputFile.bytes;
-        String text = new String(content, this.encoding ?: 'UTF-8');
+        byte[] content = inputFile.bytes
+        String text = new String(content, this.encoding ?: 'UTF-8')
 
-        CharStream charStream = CharStreams.fromReader(new StringReader(text));
-        GroovyLangLexer lexer = new GroovyLangLexer(charStream);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        GroovyLangParser parser = new GroovyLangParser(tokens);
+        CharStream charStream = CharStreams.fromReader(new StringReader(text))
+        GroovyLangLexer lexer = new GroovyLangLexer(charStream)
+        CommonTokenStream tokens = new CommonTokenStream(lexer)
+        GroovyLangParser parser = new GroovyLangParser(tokens)
+        ATNSimulator interpreter = parser.getInterpreter()
+        boolean profile = interpreter instanceof ProfilingATNSimulator
 
-        this.process(lexer, GroovyLangParser.class, parser, charStream);
+        long beginTime = System.nanoTime()
+        this.process(lexer, GroovyLangParser.class, parser, charStream)
+        long endTime = System.nanoTime()
+
+        if (profile) {
+            DecisionInfo[] decisionInfos = ((ProfilingATNSimulator) interpreter).decisionInfo
+            if (decisionInfos) {
+                println "################# BEGIN PROFILING RESULT #################"
+                println "DECISION,${'\t' * 5}TIME_IN_PREDICTION,${'\t' * 5}DETAILS"
+                long sumTimeInPrediction = 0
+                decisionInfos
+                        .sort { d1, d2 -> d2.timeInPrediction <=> d1.timeInPrediction }
+                        .each { DecisionInfo decisionInfo ->
+                    sumTimeInPrediction += decisionInfo.timeInPrediction
+                    println "${decisionInfo.decision},${'\t' * 5}${decisionInfo.timeInPrediction  / 10D**9}s,${'\t' * 5}${decisionInfo}"
+                }
+                println "SUM_TIME_IN_PREDICTION: ${sumTimeInPrediction / 10D**9}s, SUM_TIME_IN_PROCESSING: ${(endTime - beginTime) / 10D**9}s"
+                println "################# END   PROFILING RESULT #################"
+            }
+        }
     }
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         if (args.length == 0) {
-            log.info "Usage: [-tokens] [-lexer] [-tree] [-gui] [-ps file.ps] [-encoding encodingname] [-trace] [-diagnostics] [-SLL] input-filename";
-            return;
+            log.info "Usage: [-tokens] [-lexer] [-tree] [-gui] [-ps file.ps] [-encoding encodingname] [-trace] [-diagnostics] [-profile] [-SLL] input-filename"
+            return
         }
 
         if (args.every { it.startsWith('-') }) {
             log.info "input-filename is required!"
-            return;
+            return
         }
 
-        GroovyTestRig groovyTestRig = new GroovyTestRig(args);
+        if (args.contains('-profile')) {
+            System.setProperty("groovy.antlr4.profile", "true")
+        }
 
-        groovyTestRig.inspectParseTree();
+        GroovyTestRig groovyTestRig = new GroovyTestRig(args)
+
+        groovyTestRig.inspectParseTree()
     }
 }
 
