e2a00bcf944dabc1525db3b8a55af731fad173fa
Daniel.Sun <sunlan@apache.org>
2020-02-16 01:20:44 +0800

    Sync changes from apache/groovy (#68)

--- src/main/java/org/apache/groovy/parser/antlr4/Antlr4ParserPlugin.java
+++ src/main/java/org/apache/groovy/parser/antlr4/Antlr4ParserPlugin.java
@@ -18,125 +18,43 @@
  */
 package org.apache.groovy.parser.antlr4;
 
-import groovy.lang.GroovyClassLoader;
 import org.codehaus.groovy.GroovyBugError;
-import org.codehaus.groovy.ast.ClassNode;
 import org.codehaus.groovy.ast.ModuleNode;
-import org.codehaus.groovy.ast.expr.DeclarationExpression;
-import org.codehaus.groovy.ast.expr.Expression;
-import org.codehaus.groovy.ast.expr.VariableExpression;
-import org.codehaus.groovy.ast.stmt.ExpressionStatement;
-import org.codehaus.groovy.ast.stmt.Statement;
-import org.codehaus.groovy.control.CompilationFailedException;
-import org.codehaus.groovy.control.CompilerConfiguration;
-import org.codehaus.groovy.control.ErrorCollector;
 import org.codehaus.groovy.control.ParserPlugin;
 import org.codehaus.groovy.control.SourceUnit;
-import org.codehaus.groovy.control.io.ReaderSource;
 import org.codehaus.groovy.control.io.StringReaderSource;
 import org.codehaus.groovy.runtime.IOGroovyMethods;
-import org.codehaus.groovy.syntax.ParserException;
 import org.codehaus.groovy.syntax.Reduction;
 
 import java.io.IOException;
 import java.io.Reader;
-import java.security.AccessController;
-import java.security.PrivilegedAction;
-import java.util.List;
 
 /**
- * A parser plugin for the new parser
+ * A parser plugin for the new parser.
  */
 public class Antlr4ParserPlugin implements ParserPlugin {
-    private ReaderSource readerSource;
-    private CompilerConfiguration compilerConfiguration;
-
-    public Antlr4ParserPlugin(CompilerConfiguration compilerConfiguration) {
-        this.compilerConfiguration = compilerConfiguration;
-    }
 
     @Override
-    public Reduction parseCST(SourceUnit sourceUnit, Reader reader) throws CompilationFailedException {
-        ReaderSource readerSource = sourceUnit.getSource();
-
-        try (Reader sourceReader = null != readerSource ? readerSource.getReader() : null) {
-            if (null != readerSource && null != sourceReader) {
-                this.readerSource = readerSource;
-            } else {
-                this.readerSource = new StringReaderSource(IOGroovyMethods.getText(reader), sourceUnit.getConfiguration());
+    public Reduction parseCST(final SourceUnit sourceUnit, final Reader reader) {
+        if (!sourceUnit.getSource().canReopenSource()) {
+            try {
+                sourceUnit.setSource(new StringReaderSource(
+                        IOGroovyMethods.getText(reader),
+                        sourceUnit.getConfiguration()
+                ));
+            } catch (IOException e) {
+                throw new GroovyBugError("Failed to create StringReaderSource", e);
             }
-        } catch (IOException e) {
-            throw new GroovyBugError("Failed to create StringReaderSource instance", e);
         }
-
         return null;
     }
 
     @Override
-    public ModuleNode buildAST(SourceUnit sourceUnit, ClassLoader classLoader, Reduction cst) throws ParserException {
-        ReaderSource readerSource = sourceUnit.getSource();
-
-        try (Reader sourceReader = null != readerSource ? readerSource.getReader() : null) {
-            if (null == readerSource || null == sourceReader) {
-                sourceUnit.setSource(this.readerSource);
-            }
-        } catch (IOException e) {
-            sourceUnit.setSource(this.readerSource);
-        }
-
-        AstBuilder builder = new AstBuilder(sourceUnit, compilerConfiguration);
-
+    public ModuleNode buildAST(final SourceUnit sourceUnit, final ClassLoader classLoader, final Reduction cst) {
+        AstBuilder builder = new AstBuilder(sourceUnit,
+                sourceUnit.getConfiguration().isGroovydocEnabled(),
+                sourceUnit.getConfiguration().isRuntimeGroovydocEnabled()
+        );
         return builder.buildAST();
-    }
-
-    /**
-     * Create ClassNode instance for type string
-     *
-     * @param typeStr type string, e.g. List
-     * @return a {@link ClassNode} instance
-     * @since 3.0.0
-     */
-    public ClassNode makeType(String typeStr) {
-        SourceUnit sourceUnit =
-                new SourceUnit(
-                        "Script" + System.nanoTime(),
-                        "final " + typeStr + " v",
-                        compilerConfiguration,
-                        AccessController.doPrivileged((PrivilegedAction<GroovyClassLoader>) GroovyClassLoader::new),
-                        new ErrorCollector(compilerConfiguration)
-                );
-        AstBuilder builder = new AstBuilder(sourceUnit, compilerConfiguration);
-        ModuleNode moduleNode = builder.buildAST();
-
-        List<Statement> statementList = moduleNode.getStatementBlock().getStatements();
-
-        Statement statement;
-        try {
-            statement = statementList.get(0);
-        } catch (IndexOutOfBoundsException e) {
-            throw new GroovyBugError(statementList + " is empty");
-        }
-
-        if (!(statement instanceof ExpressionStatement)) {
-            throw new GroovyBugError(statement + " is not an instance of ExpressionStatement");
-        }
-
-        ExpressionStatement expressionStatement = (ExpressionStatement) statement;
-        Expression expression = expressionStatement.getExpression();
-
-        if (!(expression instanceof DeclarationExpression)) {
-            throw new GroovyBugError(expression + " is not an instance of DeclarationExpression");
-        }
-
-        DeclarationExpression declarationExpression = (DeclarationExpression) expression;
-        Expression leftExpression = declarationExpression.getLeftExpression();
-
-        if (!(leftExpression instanceof VariableExpression)) {
-            throw new GroovyBugError(leftExpression + " is not an instance of VariableExpression");
-        }
-
-        VariableExpression variableExpression = (VariableExpression) leftExpression;
-
-        return variableExpression.getType();
     }
 }
