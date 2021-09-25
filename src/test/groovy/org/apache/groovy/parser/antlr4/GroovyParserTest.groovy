ff5300ec8480a81deec4545c422fa1e92e6340f3
Daniel Sun <sunlan@apache.org>
2021-09-25 10:54:24 +0800

    Add one more test for enum declaration

--- src/test/groovy/org/apache/groovy/parser/antlr4/GroovyParserTest.groovy
+++ src/test/groovy/org/apache/groovy/parser/antlr4/GroovyParserTest.groovy
@@ -343,6 +343,7 @@ class GroovyParserTest extends GroovyTestCase {
     }
 
     void "test groovy core - EnumDeclaration"() {
+        doTest('core/EnumDeclaration_00.groovy');
         doTest('core/EnumDeclaration_01.groovy');
         doTest('core/EnumDeclaration_02.groovy', [ExpressionStatement]);
         doTest('core/EnumDeclaration_03.groovy');
